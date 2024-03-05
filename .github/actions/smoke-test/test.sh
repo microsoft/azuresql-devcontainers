abee22df395073790179c7f9ad5550d66b788c46
Carlos Robles <contact@croblesm.com>
2024-03-05 04:18:12 +0000

    Last version

--- .github/actions/smoke-test/test.sh
+++ .github/actions/smoke-test/test.sh
@@ -6,7 +6,7 @@ SRC_DIR="/tmp/${TEMPLATE_ID}"
 echo "Running Smoke Test"
 
 ID_LABEL="test-container=${TEMPLATE_ID}"
-devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} /bin/sh -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && if [ "$(id -u)" = "0" ]; then chmod +x test.sh; else sudo chmod +x test.sh; fi && ./test.sh; else ls -a; fi'
+devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} /bin/sh -c 'set -e && if [ -f "dab-mssql/test.sh" ]; then cd dab-mssql && if [ "$(id -u)" = "0" ]; then chmod +x test.sh; else sudo chmod +x test.sh; fi && ./test.sh; else ls -a; fi'
 
 # Clean up
 docker rm -f $(docker container ls -f "label=${ID_LABEL}" -q)
