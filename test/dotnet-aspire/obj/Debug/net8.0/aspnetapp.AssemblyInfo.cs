33647733a5604e8dbe7c5368344715c98bec67ab
Carlos Robles <contact@croblesm.com>
2024-07-11 20:13:47 +0000

    Updating documentation for all templates

--- test/dotnet-aspire/obj/Debug/net8.0/aspnetapp.AssemblyInfo.cs
+++ test/dotnet-aspire/obj/Debug/net8.0/aspnetapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3873405acac8da4561ec5f0c6497ec784e5b11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+311f511a8e5985f4e6acea0229096363c45d0573")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
